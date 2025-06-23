658066afaf18285cf3f08a782c6b91baed32f6f1
Naelz <nael.santos.nv11@gmail.com>
2025-06-23 15:07:50 -0300

    Initial project jest test

---
+++ src/main.ts
@@ -0,0 +1,33 @@
+/*
+Interface segregation principle (Princípio da segregação de Interface) -
+os clientes não devem ser forçados a depender de types, interfaces ou membros
+abstratos que não utilizam
+*/
+import { Messaging } from "./services/messaging";
+import { Order } from "./classes/order";
+import { Persistency } from "./services/persistency";
+import { Product } from "./classes/product";
+import { ShoppingCart } from "./classes/shopping-cart";
+import { FiftyPercentDiscount, NoDiscount, TenPercentDiscount, ThirtyPercentDiscount } from "./classes/discount";
+import { EnterpriseCustomer, IndividualCustomer } from "./classes/customer";
+
+const fiftyPercentDiscount = new FiftyPercentDiscount();
+const tenPercentDiscount = new TenPercentDiscount();
+const thirtyPercentDiscount = new ThirtyPercentDiscount();
+const noDiscount = new NoDiscount();
+const shoppingCart = new ShoppingCart(noDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+const individualCustomer = new IndividualCustomer('Nathan','Santos','000.111.000-11')
+const enterpriseCustomer = new EnterpriseCustomer('ISP SOLUTION','00011100011/0001')
+const order =  new Order(shoppingCart, messaging, persistency, enterpriseCustomer);
+
+shoppingCart.addItem(new Product('Camiseta', 49.91));
+shoppingCart.addItem(new Product('Caderno', 9));
+shoppingCart.addItem(new Product('Lápis', 1.59));
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDiscount());
+order.checkout();
+console.log(order.orderStatus)
