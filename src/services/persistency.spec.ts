9c3b43754792cba1e00a3e993e0b9f34129fdfeb
Naelz <nael.santos.nv11@gmail.com>
2025-06-25 10:24:41 -0300

    feature: initial config jest

---
+++ src/services/persistency.spec.ts
@@ -0,0 +1,13 @@
+describe('TESTANDO ALGUMA COISA', () => {
+  it('should return one', () => {
+    const number = 1;
+    expect(number).toBe(1);
+  });
+});
+
+describe('TESTANDO OUTRA COISA', () => {
+  it('should return Nathan', () => {
+    const name = 'Nathan';
+    expect(name).toBe('Nathan');
+  });
+});
