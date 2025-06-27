6fc76d971e8f82e953e397c830cedfe30b9c59b0
Naelz <nael.santos.nv11@gmail.com>
2025-06-27 09:59:34 -0300

    feature: test product

---
+++ src/classes/product.spec.ts
@@ -0,0 +1,17 @@
+import { Product } from './product';
+
+const createSut = (name: string, price: number) => {
+  return new Product(name, price)
+}
+
+describe('Product', () => {
+  afterEach(() => jest.clearAllMocks());
+
+  it('should return undefined', () => {
+    const sut = createSut('Camiseta', 59.9);
+    expect(sut).toHaveProperty('name', 'Camiseta');
+    expect(sut.price).toBeCloseTo(59.9);
+  });
+
+
+});
