259e0ee64bdaa79bd00f4eae9ea44fa31bd1eb08
Naelz <nael.santos.nv11@gmail.com>
2025-07-02 14:45:58 -0300

    Feature: Test add 2 items Shopping Cart

--- src/classes/shopping-cart.spec.ts
+++ src/classes/shopping-cart.spec.ts
@@ -1,4 +1,5 @@
 import { Discount } from './discount';
+import { CartItem } from './interfaces/cart-item';
 import { ShoppingCart } from './shopping-cart';
 
 const createSut = () => {
@@ -13,9 +14,25 @@ const createDiscountMock = () => {
 
 };
 
+const createCartItem = (name: string, price: number) => {
+  class CartItemMock implements CartItem {
+    constructor (public name: string, public price: number) {};
+  };
+  return new CartItemMock(name, price);
+};
+
 describe('Shopping Cart', () => {
   it('should be an empty cart when no product is added', () => {
     const { sut } = createSut();
     expect(sut.isEmpty()).toBe(true);
+  })
+
+    it('should have 2 carts items', () => {
+    const { sut } = createSut();
+    const cartItem1 = createCartItem('Camisa', 80);
+    const cartItem2 = createCartItem('Bermuda', 50);
+    sut.addItem(cartItem1)
+    sut.addItem(cartItem2)
+    expect(sut.items.length).toBe(2);
   })
 });
