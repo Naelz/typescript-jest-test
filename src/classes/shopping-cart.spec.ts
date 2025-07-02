d7a15de192472a9266548d00998be3acfd971876
Naelz <nael.santos.nv11@gmail.com>
2025-07-02 14:18:21 -0300

    feature: test shopping cart is empty

--- src/classes/shopping-cart.spec.ts
+++ src/classes/shopping-cart.spec.ts
@@ -1,5 +1,11 @@
+import { Discount } from './discount';
 import { ShoppingCart } from './shopping-cart';
 
 describe('Shopping Cart', () => {
+  it('should be an empty cart when no product is added', () => {
+    class DiscountMock extends Discount {}
 
+    const sut = new ShoppingCart(new DiscountMock());
+    expect(sut.isEmpty()).toBe(true);
+  })
 });
